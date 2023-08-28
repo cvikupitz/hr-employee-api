a9c2091d7b0c4f2b1a2684933a8169fa0e1ca0bb
cvikupitz <cvikupitz@gmail.com>
2023-08-28 17:33:45 -0500

    Added sql script for testing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO employee_statuses (_ID, TITLE, UPDATE_USER_ID, UPDATE_TS) VALUES
+  (101, 'Full Time', 'H2 Script', '2001-01-01 22:00:00'),
+  (102, 'Part Time', 'H2 Script', '2001-01-01 22:00:00'),
+  (103, 'Temporary', 'H2 Script', '2001-01-01 22:00:00'),
+  (104, 'Seasonal', 'H2 Script', '2001-01-01 22:00:00'),
+  (105, 'Leave of Absence', 'H2 Scripts', '2001-01-01 22:00:00'),
+  (106, 'Resigned', 'H2 Scripts', '2001-01-01 22:00:00'),
+  (107, 'Terminated', 'H2 Script', '2004-03-31 22:00:00');
+
+INSERT INTO employee_titles (_ID, TITLE, ABBREVIATION, UPDATE_USER_ID, UPDATE_TS) VALUES
+  (201, 'Chief Executive Officer', 'CEO', 'H2 Script', '2001-01-01 22:00:00'),
+  (202, 'Chief Operating Officer', 'COO', 'H2 Script', '2001-01-01 22:00:00'),
+  (203, 'Chief Financial Officer', 'CFO', 'H2 Script', '2001-01-01 22:00:00'),
+  (204, 'Chief Technology Officer', 'CTO', 'H2 Script', '2001-01-01 22:00:00'),
+  (205, 'Chief Marketing Officer', 'CMO', 'H2 Script', '2001-01-01 22:00:00'),
+  (206, 'Chief Legal Officer', 'CLO', 'H2 Script', '2001-01-01 22:00:00');
+
+INSERT INTO employee_types (_ID, TITLE, UPDATE_USER_ID, UPDATE_TS) VALUES
+  (301, 'Associate', 'H2 Script', '2001-01-01 22:00:00'),
+  (302, 'Vendor/Contractor', 'H2 Scripts', '2001-01-01 22:00:00'),
+  (303, 'Intern', 'H2 Script', '2004-03-31 22:00:00');
