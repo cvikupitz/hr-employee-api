46f2dc880bc3272550d03c056229401cbeb033dc
cvikupitz <cvikupitz@gmail.com>
2023-08-29 21:07:33 -0500

    Added new repostiories

---
+++ src/main/java/com/company/hr/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package com.company.hr.repository;
+
+import com.company.hr.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+
+}
