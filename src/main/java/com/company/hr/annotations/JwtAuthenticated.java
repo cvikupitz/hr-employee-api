25d3f7dcf7a4539832761c9bdb06927d8a38d412
cvikupitz <cvikupitz@gmail.com>
2023-09-03 10:48:50 -0500

    Added javadocs

--- src/main/java/com/company/hr/annotations/JwtAuthenticated.java
+++ src/main/java/com/company/hr/annotations/JwtAuthenticated.java
@@ -6,9 +6,34 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * <p>Annotation to put on REST controller methods that will use the JWT authentication filter. All
+ * methods with this annotation will have all requests sent through the {@link
+ * com.company.hr.filters.JwtAuthInterceptor} and have JWT authorization performed prior to
+ * processing the request. The method annotated should be a standard REST controller method
+ * annotated with any of Spring's method mapping methods.</p>
+ *
+ * <p>Example:</p>
+ * <pre>{@code
+ *   @JwtAuthenticated(ClientRole.READ_ONLY)
+ *   public ResponseEntity<String> findBookById(@PathVariable Integer id) {
+ *     ...
+ *   }
+ * }</pre>
+ *
+ * @author Cole Vikupitz
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JwtAuthenticated {
 
+  /**
+   * The minimum permission level required to access and use this endpoint. The JWT validated will
+   * contain a custom claim with the user's permission level which is matched against this
+   * requirement. Operation will be allowed only if the permission level in the JWT is greater than
+   * or equal to this level.
+   *
+   * @return The minimum permission level needed to access this operation.
+   */
   ClientRole value();
 }
--- src/main/java/com/company/hr/enums/ClientRole.java
+++ src/main/java/com/company/hr/enums/ClientRole.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 
 /**
  * Enumeration for roles assigned to clients and their provided permission/access levels.
+ *
+ * @author Cole Vikupitz
  */
 @RequiredArgsConstructor
 @Getter
@@ -12,22 +14,22 @@ public enum ClientRole {
 
   /**
    * {@code READ_ONLY}<br>
-   * <p>
-   * Read-only access; users can view but perform no edits.
+   *
+   * <p>Read-only access; users can view but perform no edits.</p>
    */
   READ_ONLY(Integer.MIN_VALUE),
 
   /**
    * {@code USER}<br>
-   * <p>
-   * User-level access; usually grants some (but not all) editing permissions.
+   *
+   * <p>User-level access; usually grants some (but not all) editing permissions.</p>
    */
   USER(0),
 
   /**
    * {@code ADMINISTRATOR}<br>
-   * <p>
-   * Admin-level access; all permissions granted.
+   *
+   * <p>Admin-level access; all permissions granted.</p>
    */
   ADMINISTRATOR(Integer.MAX_VALUE);
 
