00f77d7b84cd9331ef9544bf5ae2fd92820f512f
cvikupitz <cvikupitz@gmail.com>
2023-09-03 19:57:13 -0500

    Added employee test generation script

--- src/main/java/com/company/hr/constants/ConstraintConstants.java
+++ src/main/java/com/company/hr/constants/ConstraintConstants.java
@@ -11,6 +11,8 @@ public final class ConstraintConstants {
   public static final int SSN_CHAR_LIMIT = 11;
   public static final String SSN_REGEX = "^\\d{3}-\\d{2}-\\d{4}$";
   public static final String DATE_REGEX = "^\\d{4}-\\d{2}-\\d{2}$";
+  public static final int GENDER_CHAR_LIMIT = 1;
+  public static final String GENDER_REGEX = "^(M|F|U)$";
   public static final int ADDRESS_LINE_CHAR_LIMIT = 64;
   public static final int STATE_CHAR_LIMIT = 2;
   public static final String STATE_REGEX = "^[A-Z]{2}$";
