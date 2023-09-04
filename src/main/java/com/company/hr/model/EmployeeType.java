408d01a44ee150e029b425553ed668d621925580
cvikupitz <cvikupitz@gmail.com>
2023-09-03 22:52:52 -0500

    Fixed lombok annotations

--- src/main/java/com/company/hr/model/EmployeeType.java
+++ src/main/java/com/company/hr/model/EmployeeType.java
@@ -3,7 +3,15 @@ package com.company.hr.model;
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
 
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "employee_types")
 public class EmployeeType extends MetadataModel implements Serializable {
