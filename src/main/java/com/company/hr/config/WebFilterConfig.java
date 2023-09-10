a3d02358e346d74398b2d58297e7301962b9033c
cvikupitz <cvikupitz@gmail.com>
2023-09-10 14:51:33 -0500

    Moved jwt creation to local class

--- src/main/java/com/company/hr/config/WebFilterConfig.java
+++ src/main/java/com/company/hr/config/WebFilterConfig.java
@@ -3,21 +3,26 @@ package com.company.hr.config;
 import com.company.hr.filters.JwtAuthInterceptor;
 import com.company.hr.filters.RequestLoggerInterceptor;
 import com.company.hr.jwt.JWTAuthService;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
 @RequiredArgsConstructor
 public class WebFilterConfig implements WebMvcConfigurer {
 
-  private final JWTAuthService jwtAuthService;
-
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
+
+    JWTAuthService jwtAuthService;
+    try {
+      jwtAuthService = new JWTAuthService(System.getProperty("jwt.secret.path"));
+    } catch (IOException e) {
+      throw new IllegalStateException("Failed to instantiate the JWT authentication service,", e);
+    }
+
     registry.addInterceptor(new RequestLoggerInterceptor());
     registry.addInterceptor(new JwtAuthInterceptor(jwtAuthService));
   }
